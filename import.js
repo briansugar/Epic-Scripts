08192eb1e57e1e9fd33ae8f859df4246cfff9b70
Brian Sugar <briansugar@gmail.com>
2016-08-15 10:13:39 -0400

    plane work

--- import.js
+++ import.js
@@ -7,6 +7,8 @@ var fileType    = require('file-type');
 var mkdirp      = require('mkdirp');
 var gm          = require('gm');
 var getDuration = require('get-video-duration');
+var probe       = require('node-ffprobe');
+var titleCase   = require('title-case');
 
 var files     = [];
 var promises  = []
@@ -14,14 +16,19 @@ var promises  = []
 Parse.initialize("l0aFb4s6u3fgaLYLw9RNRumdpaKpRtvwO9oN0wnN");
 Parse.serverURL = 'https://cloud.epic.live/parse'
 
-var storyName = process.argv[2];
-var dir = process.argv[3];
+var dir = process.argv[2];
+if (!dir) {
+  console.log("Usage: node import.js Directory {Story Name}")
+  return
+}
+
+var storyName = process.argv[3] ? process.argv[3] : createStoryName(dir);
 var epicUser = {"__type": "Pointer", "className": "_User", "objectId": "sLvU1OMk2b"};
 
 if (storyName.indexOf("\/") >= 0) {
   console.log("Error: Name contains /")
-  console.log("arg1 = Story Name")
-  console.log("arg2 = Directory")
+  console.log("arg1 = Directory")
+  console.log("arg2 = Story Name")
   return
 }
 
@@ -29,12 +36,14 @@ var walker  = walk.walk(dir, { followLinks: false });
 
 walker.on('file', function(root, stat, next) {
     // Add this file to the list of files
-    files.push(root + '/' + stat.name);
+    if (stat.name.indexOf(".DS_Store") < 0) {
+      files.push(root + '/' + stat.name);
+      console.log(stat.name)
+    }
     next();
 });
 
 walker.on('end', function() {
-
   var Story = Parse.Object.extend("Story");
   var story = new Story();
   story.set("user", epicUser);
@@ -45,6 +54,7 @@ walker.on('end', function() {
     success: function (story) {
       console.log("Story Save ok");
       console.log(story.id)
+
       files.forEach(function(file){
         var buffer = readChunk.sync(file, 0, 262);
         var bufferType = fileType(buffer);
@@ -62,12 +72,13 @@ walker.on('end', function() {
           type = "image"
         }
 
-        var name = file.split("/").pop()
+        var name = getFileName(file)
         if (type == "video") {
-          promises.push(getScreenShotFromVideo(file, name, dir))
+          promises.push(addAudioIfNeeded(file))
+          promises.push(getScreenShotFromVideo(file, dir))
         }
         else if (type == "image") {
-          promises.push(reduceJpegSize(file, name, dir))
+          promises.push(reduceJpegSize(file, dir))
         }
 
         var parseFile = new Parse.File(name, {base64: base64_encode(file)});
@@ -116,16 +127,12 @@ walker.on('end', function() {
 });
 
 function saveStoryMoment(moment, story, name) {
-
-  var seconds = name.split(".").shift().split("_").pop()
-  var momentDate = getBaseMomentDate()
-  momentDate.setSeconds(momentDate.getSeconds() + 10)
   var StoryMoment = Parse.Object.extend("StoryMoments")
   var storyMoment = new StoryMoment()
   storyMoment.set("user", epicUser)
   storyMoment.set("moment", moment)
   storyMoment.set("hidden", false)
-  storyMoment.set("momentDate", momentDate)
+  storyMoment.set("momentDate", getMomentDate(name))
   storyMoment.set("views", 0)
   storyMoment.set("comments", 0)
   storyMoment.set("likes", 0)
@@ -151,6 +158,7 @@ function saveMoment(name, previewImage, parseFile, mediaFileType, duration, stor
     moment.set("mediaFileType", mediaFileType);
     moment.set("fileName", name);
     moment.set("duration", duration);
+    moment.set("momentDate", getMomentDate(name))
     moment.save().then(function() {
       console.log("moment saved " + moment.id)
 
@@ -167,17 +175,17 @@ function saveMoment(name, previewImage, parseFile, mediaFileType, duration, stor
   })
 }
 
-function getScreenShotFromVideo(file, name) {
+function getScreenShotFromVideo(file) {
   mkdirp(dir + '/thumbs', function(err) {
     return promises.push(ffmpeg(file)
       .screenshots({
         timemarks: [ '0' ],
-        filename: name.replace("mp4", "png").replace("mov", "png"),
+        filename: getFileName(file).replace("mp4", "png").replace("mov", "png"),
         folder: dir + '/thumbs/'
       })
       .on('end', function() {
-          console.log('Finished creating thumb for ' + name);
-          promises.push(makeJpeg(dir + '/thumbs/' + name.replace("mp4", "png").replace("mov", "png"), name))
+          console.log('Finished creating thumb for ' + getFileName(file));
+          promises.push(makeJpeg(dir + '/thumbs/' + getFileName(file).replace("mp4", "png").replace("mov", "png"), getFileName(file)))
       })
       .on('error', function(err, stdout, stderr) {
         console.log("ffmpeg stdout:\n" + stdout);
@@ -185,35 +193,27 @@ function getScreenShotFromVideo(file, name) {
       })
     )
   })
-  // return promise.then(function() {
-  //   var proc = new ffmpeg(file)
-  //   proc.takeScreenshots({
-  //     count: 1,
-  //     timemarks: [ '0' ],
-  //     filename: name.replace("mp4", "png").replace("mov", "png")
-  //   }, dir + '/thumbs/')
-  // });
 }
 
-function reduceJpegSize(image, name, dir) {
+function reduceJpegSize(image, dir) {
   return gm(image).setFormat("jpg").write(image, function(error){
     if (error) {
       console.log("Error: " + error)
     }
     else {
-      console.log("Finished compressing " + name);
+      console.log("Finished compressing " + getFileName(image));
     }
   });
 }
 
-function makeJpeg(image, name) {
+function makeJpeg(image) {
   if (image.indexOf(".png") > 0) {
     gm(image).setFormat("jpg").write(image.replace("png", "jpg"), function(error){
       if (error) {
         console.log(error)
       }
       else {
-        console.log("Finished converting thumb png to jpeg for " + name);
+        console.log("Finished converting thumb png to jpeg for " + getFileName(image));
         return promises.push(fs.unlinkSync(image));
       }
     });
@@ -238,4 +238,59 @@ function getBaseMomentDate() {
   var date = dir.split(" ").shift().split("/").pop()
   var pattern = /(\d{2})(\d{2})(\d{2})/;
   return new Date(date.replace(pattern,'20$3-$1-$2'));
+}
+
+function addAudioTrack(file) {
+  var fileNoSpaces = file.replace(/\ /g, "\\ ")
+  var tmpFile = fileNoSpaces.replace("IMG_", "IMG2_")
+  var exec = require('child_process').exec;
+  exec('ffmpeg -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 -i ' + fileNoSpaces + ' \
+  -shortest -c:v copy -c:a aac ' + tmpFile + '; mv ' + tmpFile + ' ' + fileNoSpaces, function(error, stdout, stderr) {
+    if (!error) {
+      console.log("Audio track added to " + getFileName(file))
+    }
+    else {
+     console.log(error)
+    }
+  });
+}
+
+function getFileName(file) {
+  return file.split("/").pop()
+}
+
+function addAudioIfNeeded(file) {
+  var needsAudioTrack = true
+  return probe(file, function(err, probeData) {
+    probeData.streams.forEach(function(stream) {
+        if (stream.codec_name == "aac") {
+          needsAudioTrack = false
+        }
+    })
+    if (needsAudioTrack) {
+      console.log("No Audio track found for " + getFileName(file))
+      addAudioTrack(file)
+    }
+    else {
+      console.log("Audio track found for " + getFileName(file))
+    }
+  });
+}
+
+function createStoryName(dir) {
+  var dirPieces = dir.split("/")
+  var name = dirPieces.pop()
+  if (name == "") {
+    name = dirPieces.pop()
+  }
+  var namePieces = name.split(" ")
+  namePieces.shift()
+  name = titleCase(namePieces.join(" "))
+  return name
+}
+
+function getMomentDate(name) {
+  var seconds = name.split(".").shift().split("_").pop()
+  var momentDate = getBaseMomentDate()
+  momentDate.setSeconds(momentDate.getSeconds() + seconds)
 }
